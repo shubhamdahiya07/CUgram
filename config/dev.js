f2d6ed24d3816fa2c6c36a5a13b91d5af4f7af3e
Himanshu Dahiya <52717705+Himanshu-Dahiya@users.noreply.github.com>
2021-05-04 18:30:08 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports={
-    mongoUrl:"mongodb+srv://anchor123:Anc123@cluster0.ugh2w.mongodb.net/IPUgram?retryWrites=true&w=majority"
-    ,secret_key:"jbljfsnv",
-    SENDGRID_API:"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",
-    EMAIL:"http://localhost:3000"
+    mongoUrl:""
+    ,secret_key:"",
+    SENDGRID_API:"",
+    EMAIL:""
 }
